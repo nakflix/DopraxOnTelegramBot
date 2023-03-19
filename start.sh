554771e0d8a8e0d94789d3adb6612b0380e891a6
nakflix <123237114+nakflix@users.noreply.github.com>
2023-03-19 12:28:38 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-git clone https://github.com/nakflix/FileSharing1 ok && cd ok && pip3 install -U -r requirements.txt
+git clone https://github.com/nakflix/Filesharing1bot ok && cd ok && pip3 install -U -r requirements.txt
 python3 main.py
